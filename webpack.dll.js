7c7986f1d2b8f0ca92df09492bb59dac1890f313
杨杰 <admin@yanglaowu.cc>
2020-07-24 10:09:17 +0800

    update

--- webpack.dll.js
+++ webpack.dll.js
@@ -7,8 +7,8 @@ module.exports = {
   mode: "production",
   entry: {
     vue: ["vue", "vuex", "vue-router"], // 抽离vue 核心库
-    element: ["element-ui"], // 抽离ui库, css由于做了按需加在 不用单独处理
-    common: [path.join(__dirname, "./src/styles/index.scss")] // 抽离全局样式
+    element: ["element-ui"] // 抽离ui库, css由于做了按需加在 不用单独处理
+    // common: [path.join(__dirname, "./src/styles/index.scss")] // 抽离全局样式
   },
   output: {
     path: path.resolve(__dirname, "./vendors"),
