b300af283f9bf5e66975e13ae043036d84f31017
杨杰 <admin@yanglaowu.cc>
2020-07-24 08:52:02 +0800

    update

--- webpack.dll.js
+++ webpack.dll.js
@@ -13,7 +13,9 @@ module.exports = {
     filename: "[name].dll.js",
     library: "[name]"
   },
-
+  performance: {
+    hints: false
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DllPlugin({
