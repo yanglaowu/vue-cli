ee797bcaa635a60ce5fb1a19bdda43b63c2654d8
杨杰 <admin@yanglaowu.cc>
2020-07-24 10:37:43 +0800

    update

--- webpack.dll.js
+++ webpack.dll.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: "production",
   entry: {
     vue: ["vue", "vuex", "vue-router"], // 抽离vue 核心库
-    element: ["element-ui"] // 抽离ui库, css由于做了按需加在 不用单独处理
+    vendor: ["element-ui", "axios", "lodash", "normalize.css"] // 抽离三方库
     // common: [path.join(__dirname, "./src/styles/index.scss")] // 抽离全局样式
   },
   output: {
