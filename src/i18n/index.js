18c9c622370ff7b35506c0361b224a567be6f4dd
yanglaowu <admin@yanglaowu.cc>
2020-09-08 09:42:50 +0800

    update

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,14 +2,14 @@
  * @Author: yangjie
  * @Date: 2020-09-04 14:52:35
  * @Last Modified by: yangjie
- * @Last Modified time: 2020-09-04 14:52:55
+ * @Last Modified time: 2020-09-08 09:42:26
  * 国际化设置
  */
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 
-import en_US from "./locale/en_US";
-import zh_CN from "./locale/zh_CN";
+import en_US from "./lang/en_US";
+import zh_CN from "./lang/zh_CN";
 
 Vue.use(VueI18n);
 
